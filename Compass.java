660474665139f9d04c3a56e3221e24d4c945863a
Python <diegorscorpion@gmail.com>
2015-03-28 17:58:23 -0600

    Mejora
    Se corrigió la sintaxis de la declaración del sensor

--- Compass.java
+++ Compass.java
@@ -6,14 +6,20 @@ import lejos.nxt.*;
 
 public class Compass
 {
+
 	public static void main(String[] args)
 	{
-			Compass sensor=new Compass sensor(SensorPort.S2);
-			sensor.resetCartesianZero();
-			while(true){
-				LCD.drawInt(sensor.getDregrees(),1,1);
-				LCD.drawInt(sensor.getDegreesCartesian(),1,3);		
-			} 
+	
+		CompassHTSensor sensor = new CompassHTSensor(SensorPort.S2);
+		sensor.resetCartesianZero();
+	
+		while(true)
+		{
+			
+			LCD.drawInt(sensor.getDregrees(),1,1);
+			LCD.drawInt(sensor.getDegreesCartesian(),1,3);		
+			
+		} 
 				
 	}
 
