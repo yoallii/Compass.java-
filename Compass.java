e021ea676a15781769590833b30ddbef58ef1bb4
Franco (Choco) <gfdgeras@gmail.com>
2015-03-28 19:34:52 -0600

    Cambio mio
    Cambio por yoyotl

--- Compass.java
+++ Compass.java
@@ -9,46 +9,14 @@ public class Compass
 
 	public static void main(String[] args)
 	{
-	
 		CompassHTSensor sensor = new CompassHTSensor(SensorPort.S2);
 		sensor.resetCartesianZero();
 	
 		while(true)
 		{
 		
-			LCD.drawInt((int)sensor.getDegreesCartesian(),1,1);
-			LCD.drawInt((int)sensor.getDegreesCartesian(),1,3);
-
-			if (sensor == 0)
-			{
-			
-				Motor.A.setSpeed(900);
-				Motor.A.forward();
-				Motor.C.setSpeed(900);
-				Motor.C.forward();
-
-			}
-
-			if (sensor >= 0)
-			{
-
-				Motor.A.setSpeed(450);
-				Motor.A.backward();
-				Motor.C.setSpeed(450);
-				Motor.C.forward();
-
-			}
-
-			if (sensor <= 0)
-			{
-
-				Motor.C.setSpeed(450);
-				Motor.C.forward();
-				Motor.A.setSpeed(450);
-				Motor.A.backward();
-
-			}
-			
+			LCD.drawInt((int)sensor.getDegreesCartesian(),1,1);		
+			LCD.clear();	
 		} 
 				
 	}
