f76e88d7013a3cbdc333b0ab908c598b6a37b929
Python <diegorscorpion@gmail.com>
2015-03-28 19:17:27 -0600

    Alineación
    Movimientos posibles por si se desalinea

--- Compass.java
+++ Compass.java
@@ -16,7 +16,38 @@ public class Compass
 		while(true)
 		{
 		
-			LCD.drawInt((int)sensor.getDegreesCartesian(),1,1);		
+			LCD.drawInt((int)sensor.getDegreesCartesian(),1,1);
+			LCD.drawInt((int)sensor.getDegreesCartesian(),1,3);
+
+			if (sensor == 0)
+			{
+			
+				Motor.A.setSpeed(900);
+				Motor.A.forward();
+				Motor.C.setSpeed(900);
+				Motor.C.forward();
+
+			}
+
+			if (sensor <= 0)
+			{
+
+				Motor.A.setSpeed(450);
+				Motor.A.forward();
+				Motor.C.setSpeed(450);
+				Motor.C.backward();
+
+			}
+
+			if (sensor >= 0)
+			{
+
+				Motor.A.setSpeed(450);
+				Motor.A.backward();
+				Motor.C.setSpeed(450);
+				Motor.C.forward();
+
+			}
 			
 		} 
 				
